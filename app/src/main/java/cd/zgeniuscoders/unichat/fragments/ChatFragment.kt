916d4a77f83095701bfb0dd4fdf28af947b4ea43
=zgeniuscoders <=zgeniuscoders@gmail.com>
2023-04-05 14:26:52 +0200

    chat fragment

---
+++ app/src/main/java/cd/zgeniuscoders/unichat/fragments/ChatFragment.kt
@@ -0,0 +1,23 @@
+package cd.zgeniuscoders.unichat.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cd.zgeniuscoders.unichat.R
+import cd.zgeniuscoders.unichat.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment() {
+
+    private lateinit var binding: FragmentChatBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
