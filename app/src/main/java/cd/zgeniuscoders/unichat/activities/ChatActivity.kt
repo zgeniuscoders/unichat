755c8f041e2d3b0b49b558d47b7f6fb8bed0837b
=zgeniuscoders <=zgeniuscoders@gmail.com>
2023-04-05 15:18:33 +0200

    ui chat

--- app/src/main/java/cd/zgeniuscoders/unichat/activities/ChatActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/unichat/activities/ChatActivity.kt
@@ -2,10 +2,13 @@ package cd.zgeniuscoders.unichat.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cd.zgeniuscoders.unichat.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
